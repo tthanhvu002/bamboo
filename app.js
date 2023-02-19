c9fc3a6b6be9f341c7d8c510eeb39e796d5786df
tthanhvu002 <81304749+tthanhvu002@users.noreply.github.com>
2023-02-19 10:47:37 +0700

    pj

---
+++ app.js
@@ -0,0 +1,11 @@
+const slider = document.querySelector('.slider')
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+const width = document.querySelector('.slider .item').offsetWidth
+nextBtn.onclick = () => {
+  slider.scrollLeft += width
+}
+prevBtn.onclick = () => {
+  console.log(2);
+  slider.scrollLeft -= width
+}
